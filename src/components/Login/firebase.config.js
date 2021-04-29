9744fb4056c312f5ba83c28517d0ce979b4b0399
akashtech-source <mr.ifranakash@gmail.com>
2021-04-29 18:13:23 +0600

    login

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBVwYdFnNK6vIKGwW1FyiTVguQBLG4yIRA",
-    authDomain: "coding-mate-it.firebaseapp.com",
-    projectId: "coding-mate-it",
-    storageBucket: "coding-mate-it.appspot.com",
-    messagingSenderId: "414324022541",
-    appId: "1:414324022541:web:e4c0ec92edf2e5de302444",
-    measurementId: "G-37M61E480W"
-  };
+  apiKey: "AIzaSyB4IiF9KZEbtZ6p1kAc_rFritUXiE4hppA",
+  authDomain: "the-rich-table.firebaseapp.com",
+  projectId: "the-rich-table",
+  storageBucket: "the-rich-table.appspot.com",
+  messagingSenderId: "132829203691",
+  appId: "1:132829203691:web:daef071eafb32ea02e42ae"
+};
 
   export default firebaseConfig;
